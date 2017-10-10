5d6cc43dfc58a3b8c7c30c96dce3c0a4ae667bf7
Fonix Hardcastle <me@fonix.online>
2017-10-10 11:23:24 +0100

    Slight Clean!

--- use.sh
+++ use.sh
@@ -3,25 +3,25 @@ sleep 2
 
 echo "Checking the APT repo's"
 sleep 2
-sudo apt-get update -y
-sudo apt-get install nfs-kernel-server -y
-sudo apt-get install nfs-common -y
+    sudo apt-get update -y
+    sudo apt-get install nfs-kernel-server -y
+    sudo apt-get install nfs-common -y
 echo "Setup of APT done!"
 sleep 5
 
 echo "Create the maps folder and setup the mountpoint!"
-mkdir -p /mnt/maps
-mount 10.50.0.104:/fs-data/pool/maps /mnt/maps/
+    mkdir -p /mnt/maps
+    mount 10.50.0.104:/fs-data/pool/maps /mnt/maps/
 echo "Completed (Hopefully you didnt see any errors from that!"
 sleep 2
 
 echo "Adding the NFS share to Fstab!"
-echo "10.50.0.104:/fs-data/pool/maps    /mnt/maps   nfs   rsize=8192,wsize=8192,timeo=14,intr" >> /etc/fstab
+    echo "10.50.0.104:/fs-data/pool/maps    /mnt/maps   nfs   rsize=8192,wsize=8192,timeo=14,intr" >> /etc/fstab
 sleep 1
 echo "Done"
 
 echo "Copy over the Daemon changes to allow Flo to do its magic <3"
-cp /mnt/maps/Docker-Edits/docker.js /srv/daemon/src/controllers/docker.js
+    cp /mnt/maps/Docker-Edits/docker.js /srv/daemon/src/controllers/docker.js
 sleep 1
 echo "Done!"
 
